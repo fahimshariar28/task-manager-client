4543a270fbef50df53da4f61f1b07bfa54867ee6
Fahim Shariar <fahimshariar28@gmail.com>
2023-06-14 14:00:51 +0600

    Tyour task page

--- src/pages/YourTask/YourTask.jsx
+++ src/pages/YourTask/YourTask.jsx
@@ -40,32 +40,34 @@ const YourTask = () => {
   return (
     <div>
       <h2 className="text-2xl text-center">Your Tasks</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5">
-        {tasks.map((task) => (
-          <div key={task._id} className="card  bg-base-200 ">
-            <div className="card-body">
-              <h2 className="card-title">{task.taskName}</h2>
-              <p>{task.taskDescription}</p>
-              <p>{task.date}</p>
-              <div className="card-actions justify-end">
-                {task.status === "incomplete" ? (
-                  <p className="text-red-600">Incomplete</p>
-                ) : (
-                  <p className="text-primary">Complete</p>
-                )}
-                {task.status === "incomplete" && (
-                  <button
-                    onClick={() => handleComplete(task._id)}
-                    className="btn btn-primary"
-                  >
-                    Mark as Complete
-                  </button>
-                )}
+      {
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5">
+          {tasks.map((task) => (
+            <div key={task._id} className="card  bg-base-200 ">
+              <div className="card-body">
+                <h2 className="card-title">{task.taskName}</h2>
+                <p>{task.taskDescription}</p>
+                <p>{task.date}</p>
+                <div className="card-actions justify-end">
+                  {task.status === "incomplete" ? (
+                    <p className="text-red-600">Incomplete</p>
+                  ) : (
+                    <p className="text-primary">Complete</p>
+                  )}
+                  {task.status === "incomplete" && (
+                    <button
+                      onClick={() => handleComplete(task._id)}
+                      className="btn btn-primary"
+                    >
+                      Mark as Complete
+                    </button>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      }
     </div>
   );
 };
